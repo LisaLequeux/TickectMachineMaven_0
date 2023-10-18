4c0ecc42a03137fc932d1b36b2cf056626603221
LisaLequeux <147843628+LisaLequeux@users.noreply.github.com>
2023-10-18 17:56:48 +0200

    Update TicketMachineTest.java

--- src/test/java/ticketmachine/TicketMachineTest.java
+++ src/test/java/ticketmachine/TicketMachineTest.java
@@ -30,4 +30,54 @@ class TicketMachineTest {
 		assertEquals(10 + 20, machine.getBalance(), "La balance n'est pas correctement mise à jour");              
 	}
 
+	@Test
+	// S3 : on n’imprime pas leticket si le montant inséré est insuffisant
+	void notPrintTicketIfMoneyIsInsufficient(){
+		machine.insertMoney(PRICE-1);
+		assertFalse(machine.printTicket(),
+				"Le payement n'est pas effectué donc on n'imprime pas de ticket");
+	}
+
+	@Test
+	// S4 : on imprime le ticket si le montant inséré est suffisant
+	void printTicketIfMoneyIsSufficient(){
+		machine.insertMoney(PRICE+1);
+		assertTrue(machine.printTicket(), "Payement effectué donc impression du ticket");
+	}
+
+	@Test
+	// S5 : Quand on imprime un ticket la balance est décrémentée du prix du ticket
+	void decreasePriceOnBalance(){
+		machine.insertMoney(40);
+		machine.insertMoney(20); //on a 60 donc plus que le prix
+		machine.printTicket();
+		assertEquals(60-PRICE, machine.getBalance(),
+				"La balance a été correctement effectuée");
+	}
+
+	@Test
+	// S6 : le montant collecté est mis à jour quand on imprime un ticket (pas avant)
+	void BalanceUpdatePrintTicket(){
+		machine.insertMoney(70);
+		machine.printTicket();
+		assertEquals(PRICE, machine.getTotal()); // on doit obtenir 50
+	}
+
+	@Test
+	// S7 : refund()rendcorrectement la monnaie
+	void returnMoney(){
+		machine.insertMoney(70);
+		machine.printTicket();
+		assertEquals(machine.getBalance(), machine.refund()); // on doit obtenir 20
+	}
+
+	@Test
+	// S8 : refund()remet la balance à zéro
+	void pullBalanceToNull(){
+		machine.insertMoney(70);
+		machine.printTicket();
+		machine.refund();
+		assertEquals(0, machine.getBalance());
+	}
+
 }
